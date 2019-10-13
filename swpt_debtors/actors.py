5ba9689f2abea8e22158acf9641f15c13ae74701
Evgeni Pandurski <epandurski@gmail.com>
2019-10-13 15:00:49 +0300

    Start work of actors and models

--- swpt_debtors/actors.py
+++ swpt_debtors/actors.py
@@ -1,7 +1,24 @@
-from .extensions import broker
+import iso8601
+from .extensions import broker, APP_QUEUE_NAME
+from . import procedures
 
-# Define your asynchronous actors here. For example:
-#
-#     @broker.actor
-#     def process_job(param):
-#         pass
+
+@broker.actor(queue_name=APP_QUEUE_NAME)
+def create_debtor(debtor_id: int) -> None:
+    """Creates a new debtor."""
+
+
+@broker.actor(queue_name=APP_QUEUE_NAME)
+def terminate_debtor(debtor_id: int) -> None:
+    """Permanently terminates a debtor."""
+
+
+@broker.actor(queue_name=APP_QUEUE_NAME)
+def update_debtor_balance(
+        debtor_id: int,
+        balance: int,
+        status: int,
+        update_ts: str) -> None:
+    """Updates the balance of the debtor's account."""
+
+    update_ts = iso8601.parse_date(update_ts)
--- swpt_debtors/models.py
+++ swpt_debtors/models.py
@@ -1,3 +1,56 @@
-from .extensions import db
+import datetime
+import dramatiq
+from marshmallow import Schema, fields
+from sqlalchemy.dialects import postgresql as pg
+from sqlalchemy.sql.expression import func, null, or_, and_
+from .extensions import db, broker, MAIN_EXCHANGE_NAME
 
-# Define your database models here by sub-classing `db.Model`.
+MIN_INT32 = -1 << 31
+MAX_INT32 = (1 << 31) - 1
+MIN_INT64 = -1 << 63
+MAX_INT64 = (1 << 63) - 1
+
+
+def get_now_utc():
+    return datetime.datetime.now(tz=datetime.timezone.utc)
+
+
+class Signal(db.Model):
+    __abstract__ = True
+
+    # TODO: Define `send_signalbus_messages` class method, set
+    #      `ModelClass.signalbus_autoflush = False` and
+    #      `ModelClass.signalbus_burst_count = N` in models.
+
+    queue_name = None
+
+    @property
+    def event_name(self):  # pragma: no cover
+        model = type(self)
+        return f'on_{model.__tablename__}'
+
+    def send_signalbus_message(self):  # pragma: no cover
+        model = type(self)
+        if model.queue_name is None:
+            assert not hasattr(model, 'actor_name'), \
+                'SignalModel.actor_name is set, but SignalModel.queue_name is not'
+            actor_name = self.event_name
+            routing_key = f'events.{actor_name}'
+        else:
+            actor_name = model.actor_name
+            routing_key = model.queue_name
+        data = model.__marshmallow_schema__.dump(self)
+        message = dramatiq.Message(
+            queue_name=model.queue_name,
+            actor_name=actor_name,
+            args=(),
+            kwargs=data,
+            options={},
+        )
+        broker.publish_message(message, exchange=MAIN_EXCHANGE_NAME, routing_key=routing_key)
+
+
+class ChangedDebtorInfoSignal(Signal):
+    """Sent when debtor's principal information has changed."""
+
+    debtor_id = db.Column(db.BigInteger, primary_key=True)
