17614e70020f972ff1e2f181407884327bccff99
Evgeni Pandurski <epandurski@gmail.com>
2019-10-15 19:47:25 +0300

    Minor refactoring

--- swpt_debtors/procedures.py
+++ swpt_debtors/procedures.py
@@ -8,22 +8,20 @@ from .models import Limit
 T = TypeVar('T')
 atomic: Callable[[T], T] = db.atomic
 
-BEGINNING_OF_TIME = date(1900, 1, 1)
 
-
-def _add_limit_to_list(limits: List[Limit], new_limit: Limit, *, lower_limit=False, upper_limit=False) -> List[Limit]:
-    assert lower_limit or upper_limit
+def _add_limit_to_list(l: List[Limit], new_limit: Limit, *, lower_limit=False, upper_limit=False) -> None:
+    assert lower_limit or upper_limit, 'the limit type must be specified when calling _add_limit_to_list()'
     assert not (lower_limit and upper_limit)
 
-    def get_restrictiveness(l: Limit) -> Real:
-        return l.value if lower_limit else -l.value
+    def get_restrictiveness(limit: Limit) -> Real:
+        return limit.value if lower_limit else -limit.value
 
     def apply_eliminator(limits: List[Limit], eliminator: Limit) -> List[Limit]:
         """Remove the limits rendered ineffectual by the `eliminator`."""
 
         r = get_restrictiveness(eliminator)
-        c = eliminator.cutoff
-        return [l for l in limits if get_restrictiveness(l) > r or l.cutoff > c]
+        cutoff = eliminator.cutoff
+        return [limit for limit in limits if get_restrictiveness(limit) > r or limit.cutoff > cutoff]
 
     def find_eliminator_in_sorted_limits(sorted_limits: List[Limit]) -> Optional[Limit]:
         """Try to find a limit that makes some of the other limits ineffectual."""
@@ -36,11 +34,13 @@ def _add_limit_to_list(limits: List[Limit], new_limit: Limit, *, lower_limit=Fal
             restrictiveness = r
         return None
 
+    limits = l
     while True:
         limits = apply_eliminator(limits, new_limit)
         limits.append(new_limit)
-        limits.sort(key=lambda l: l.cutoff)
+        limits.sort(key=lambda limit: limit.cutoff)
         new_limit = find_eliminator_in_sorted_limits(limits)
         if not new_limit:
             break
-    return limits
+    l.clear()
+    l.extend(limits)
--- tests/test_swpt_debtors.py
+++ tests/test_swpt_debtors.py
@@ -11,13 +11,13 @@ def test_version(db_session):
 
 
 def test_add_limit_to_list():
-    limits1 = [
+    limits = [
         Limit(10, A_DATE, date(2000, 1, 1)),
         Limit(20, A_DATE, date(2000, 1, 2)),
     ]
-    limits2 = p._add_limit_to_list(limits1, Limit(30, A_DATE, date(2000, 1, 3)), upper_limit=True)
-    assert [l.value for l in limits2] == [10, 20, 30]
-    limits3 = p._add_limit_to_list(limits2, Limit(25, A_DATE, date(2000, 1, 4)), upper_limit=True)
-    assert [l.value for l in limits3] == [10, 20, 25]
-    limits4 = p._add_limit_to_list(limits3, Limit(30, A_DATE, date(2000, 1, 3)), upper_limit=True)
-    assert [l.value for l in limits4] == [10, 20, 25]
+    p._add_limit_to_list(limits, Limit(30, A_DATE, date(2000, 1, 3)), upper_limit=True)
+    assert [l.value for l in limits] == [10, 20, 30]
+    p._add_limit_to_list(limits, Limit(25, A_DATE, date(2000, 1, 4)), upper_limit=True)
+    assert [l.value for l in limits] == [10, 20, 25]
+    p._add_limit_to_list(limits, Limit(30, A_DATE, date(2000, 1, 3)), upper_limit=True)
+    assert [l.value for l in limits] == [10, 20, 25]
