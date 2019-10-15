a2e808885909dcab5fad3cfb52e8f3d17fd3f2fd
Evgeni Pandurski <epandurski@gmail.com>
2019-10-15 19:03:06 +0300

    Implement _add_limit_to_list

--- swpt_debtors/procedures.py
+++ swpt_debtors/procedures.py
@@ -1,11 +1,42 @@
+import math
+from datetime import date
+from numbers import Real
+from typing import TypeVar, List, Optional, Callable
 from .extensions import db
+from .models import Limit
 
-# Define your database procedures here. For example:
-#
-#     @db.atomic
-#     def do_public_stuff(**kw):
-#         pass
-#
-#
-#     def _do_private_stuff(**kw):
-#         pass
+T = TypeVar('T')
+atomic: Callable[[T], T] = db.atomic
+
+BEGINNING_OF_TIME = date(1900, 1, 1)
+
+
+def _add_limit_to_list(limits: List[Limit], new_limit: Limit, *, lower_limit=False, upper_limit=False) -> List[Limit]:
+    assert lower_limit or upper_limit
+    assert not (lower_limit and upper_limit)
+
+    def get_restrictiveness(l: Limit) -> Real:
+        return l.value if lower_limit else -l.value
+
+    def filter_by_champion(champion: Limit) -> List[Limit]:
+        r = get_restrictiveness(champion)
+        c = champion.cutoff
+        return [l for l in limits if get_restrictiveness(l) > r or l.cutoff > c]
+
+    def find_champion_in_sorted_limits(sorted_limits: List[Limit]) -> Optional[Limit]:
+        restrictiveness = math.inf
+        for champion in sorted_limits:
+            r = get_restrictiveness(champion)
+            if r >= restrictiveness:
+                return champion
+            restrictiveness = r
+        return None
+
+    while True:
+        limits = filter_by_champion(new_limit)
+        limits.append(new_limit)
+        limits.sort(key=lambda l: l.cutoff)
+        new_limit = find_champion_in_sorted_limits(limits)
+        if not new_limit:
+            break
+    return limits
--- tests/test_swpt_debtors.py
+++ tests/test_swpt_debtors.py
@@ -1,5 +1,23 @@
+from datetime import date
 from swpt_debtors import __version__
+from swpt_debtors.models import Limit
+from swpt_debtors import procedures as p
+
+A_DATE = date(1900, 1, 1)
 
 
 def test_version(db_session):
     assert __version__
+
+
+def test_add_limit_to_list():
+    limits1 = [
+        Limit(10, A_DATE, date(2000, 1, 1)),
+        Limit(20, A_DATE, date(2000, 1, 2)),
+    ]
+    limits2 = p._add_limit_to_list(limits1, Limit(30, A_DATE, date(2000, 1, 3)), upper_limit=True)
+    assert [l.value for l in limits2] == [10, 20, 30]
+    limits3 = p._add_limit_to_list(limits2, Limit(25, A_DATE, date(2000, 1, 4)), upper_limit=True)
+    assert [l.value for l in limits3] == [10, 20, 25]
+    limits4 = p._add_limit_to_list(limits3, Limit(30, A_DATE, date(2000, 1, 3)), upper_limit=True)
+    assert [l.value for l in limits4] == [10, 20, 25]
