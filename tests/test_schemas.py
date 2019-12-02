5f15a0fc146db550e920cf201f11b90437210c3a
Evgeni Pandurski <epandurski@gmail.com>
2019-12-02 21:19:14 +0200

    Add test

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -3,7 +3,7 @@ from swpt_debtors import schemas
 from swpt_debtors.lower_limits import LowerLimit
 
 
-def test_interest_rate_lower_limit():
+def test_interest_rate_lower_limit_schema():
     s = schemas.InterestRateLowerLimitSchema()
     data = s.load({'value': 5.6, 'enforcedUntil': '2020-10-25'})
     assert isinstance(data, LowerLimit)
@@ -12,10 +12,28 @@ def test_interest_rate_lower_limit():
     assert s.dump(data) == {'value': 5.6, 'enforcedUntil': '2020-10-25'}
 
 
-def test_balance_lower_limit():
+def test_balance_lower_limit_schema():
     s = schemas.BalanceLowerLimitSchema()
     data = s.load({'value': 1000, 'enforcedUntil': '2020-10-25'})
     assert isinstance(data, LowerLimit)
     assert data.value == 1000
     assert data.cutoff == date(2020, 10, 25)
     assert s.dump(data) == {'value': 1000, 'enforcedUntil': '2020-10-25'}
+
+
+def test_debtor_policy_update_request_schema():
+    s = schemas.DebtorPolicyUpdateRequestSchema()
+    data = s.load({})
+    assert data['balance_lower_limits'] == []
+    assert data['interest_rate_lower_limits'] == []
+    assert data['interest_rate_target'] is None
+    data = s.load({
+        'balanceLowerLimits': [{'value': 1000, 'enforcedUntil': '2020-10-25'}],
+        'interestRateLowerLimits': [{'value': 5.6, 'enforcedUntil': '2020-10-25'}],
+        'interestRateTarget': 6.1,
+    })
+    assert len(data['balance_lower_limits']) == 1
+    assert data['balance_lower_limits'][0].value == 1000
+    assert len(data['interest_rate_lower_limits']) == 1
+    assert data['interest_rate_lower_limits'][0].value == 5.6
+    assert data['interest_rate_target'] == 6.1
