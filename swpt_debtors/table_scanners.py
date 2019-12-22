573c5fec735f58ac12b737d0873f88bd8330f594
Evgeni Pandurski <epandurski@gmail.com>
2019-12-22 19:46:35 +0200

    Beautify

--- swpt_debtors/table_scanners.py
+++ swpt_debtors/table_scanners.py
@@ -24,11 +24,11 @@ class CachedInterestRate(NamedTuple):
 class RunningTransfersCollector(TableScanner):
     table = RunningTransfer.__table__
     columns = [RunningTransfer.debtor_id, RunningTransfer.transfer_uuid, RunningTransfer.finalized_at_ts]
+    pk = tuple_(table.c.debtor_id, table.c.transfer_uuid)
 
     def __init__(self):
         super().__init__()
         self.signalbus_max_delay = timedelta(days=current_app.config['APP_SIGNALBUS_MAX_DELAY_DAYS'])
-        self.pk = tuple_(self.table.c.debtor_id, self.table.c.transfer_uuid)
 
     def process_rows(self, rows):
         cutoff_ts = datetime.now(tz=timezone.utc) - self.signalbus_max_delay
