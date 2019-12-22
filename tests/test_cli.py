c781b4e40bec9171caa4fbf673b13a32990df399
Evgeni Pandurski <epandurski@gmail.com>
2019-12-22 16:23:17 +0200

    Improve test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -29,8 +29,12 @@ def test_purge_deleted_account(app_unsafe_session):
     from swpt_debtors.models import PurgeDeletedAccountSignal
 
     past_ts = datetime(1900, 1, 1, tzinfo=timezone.utc)
+    future_ts = datetime(2100, 1, 1, tzinfo=timezone.utc)
     app = app_unsafe_session
-    account = Account(
+    Account.query.delete()
+    PurgeDeletedAccountSignal.query.delete()
+    db.session.commit()
+    db.session.add(Account(
         debtor_id=1,
         creditor_id=2,
         change_seqnum=0,
@@ -40,19 +44,31 @@ def test_purge_deleted_account(app_unsafe_session):
         interest_rate=0.0,
         last_outgoing_transfer_date=past_ts,
         status=Account.STATUS_DELETED_FLAG | Account.STATUS_SCHEDULED_FOR_DELETION_FLAG
-    )
-    db.session.add(account)
+    ))
+    db.session.add(Account(
+        debtor_id=11,
+        creditor_id=22,
+        change_seqnum=0,
+        change_ts=future_ts,
+        principal=0,
+        interest=0.0,
+        interest_rate=0.0,
+        last_outgoing_transfer_date=past_ts,
+        status=Account.STATUS_DELETED_FLAG | Account.STATUS_SCHEDULED_FOR_DELETION_FLAG
+    ))
     db.session.commit()
     db.engine.execute('ANALYZE account')
-    assert len(Account.query.all()) == 1
-    n = len(PurgeDeletedAccountSignal.query.all())
+    assert len(Account.query.all()) == 2
+    assert len(PurgeDeletedAccountSignal.query.all()) == 0
     runner = app.test_cli_runner()
     result = runner.invoke(args=['swpt_debtors', 'scan_accounts', '--days', '0.000001', '--quit-early'])
     assert result.exit_code == 0
-    assert len(Account.query.all()) == 0
+    assert len(Account.query.all()) == 1
     purge_signals = PurgeDeletedAccountSignal.query.all()
-    assert len(purge_signals) == n + 1
-    ps = purge_signals[n]
+    assert len(purge_signals) == 1
+    ps = purge_signals[0]
     assert ps.debtor_id == 1
     assert ps.creditor_id == 2
     assert ps.if_deleted_before > past_ts
+    Account.query.delete()
+    db.session.commit()
