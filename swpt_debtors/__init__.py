142e77fbe0e30d541eb5fe5bc77de81f083e0de4
Evgeni Pandurski <epandurski@gmail.com>
2026-04-01 18:40:35 +0300

    Improve SIGTERM handling

--- swpt_debtors/__init__.py
+++ swpt_debtors/__init__.py
@@ -307,5 +307,6 @@ configure_logging(
     associated_loggers=os.environ.get("APP_ASSOCIATED_LOGGERS", "").split(),
 )
 sys.excepthook = _excepthook
-signal.signal(signal.SIGTERM, _raise_keyboard_interrupt)
-signal.signal(signal.SIGINT, _raise_keyboard_interrupt)
+
+if signal.getsignal(signal.SIGTERM) in [signal.SIG_IGN, signal.SIG_DFL, None]:
+    signal.signal(signal.SIGTERM, _raise_keyboard_interrupt)
